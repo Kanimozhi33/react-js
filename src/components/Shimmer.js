1988615fcf56cb4f08a6d87121f59de887379864
kanimozhi33 <kanimozhi02vm@gmail.com>
2025-04-10 23:22:04 +0530

    shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,16 +1,41 @@
 const Shimmer = () => {
-    return (<div className="shimmer-container">
-        <div className="">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
-        <div className="shimmer-cards">cards</div>
+    return (<div className="shimmer-container p-10 flex flex-wrap gap-5">
+        <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
+            <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
+            <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
+            <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
+            <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
+            <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
+            <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
+            <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
+            <div className="h-95 p-3 overflow-hidden bg-gray-300 shadow-2xl w-60
+        rounded-2xl animate-pulse">
+        <div className="h-35 w-55 bg-gray-100 rounded-2xl ">
+            </div></div>
         
         </div>
     );         
