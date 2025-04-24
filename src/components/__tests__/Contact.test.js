119f03486d5b45c05e28fd07d0199a137a23bd5f
kanimozhi33 <kanimozhi02vm@gmail.com>
2025-04-24 22:27:21 +0530

    end of part 2

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
-import "testing-library/jest-dom";
+import "@testing-library/jest-dom";
 test ("Should load contact us component", () => {
 
 
@@ -10,4 +10,33 @@ test ("Should load contact us component", () => {
     expect(heading).toBeInTheDocument();
 
 });
+test ("Should load button inside contact us component", () => {
 
+
+    render(<Contact/>);
+    const button  = screen.getByText("submit");
+
+    expect(button).toBeInTheDocument();
+
+});
+
+test ("Should load input name inside contact us component", () => {
+
+
+    render(<Contact/>);
+
+
+    const inputname  = screen.getByPlaceholderText("Name");
+
+    expect(inputname).toBeInTheDocument();
+
+});
+
+test("should load 2 input boxes on the contact component", () =>{
+    render(<Contact/>);
+    const inputboxes = screen.getAllByRole("textbox");
+    console.log(inputboxes);
+
+    // expect(inputboxes).toBeInTheDocument();
+
+})
